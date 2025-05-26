61e0c3df3bffac4d48b92d1ceb3694a0e6949417
Jaap <c5sb56cg@duck.com>
2025-05-26 17:08:53 +0200

    Parser Update

--- RawRealism.Parser/Program.cs
+++ RawRealism.Parser/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace RawRealism.Parser
+﻿using System;
+using System.IO;
+
+namespace RawRealism.Parser;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        // Pad naar de www-map van het hoofdproject (relatief vanaf de repo-root)
+        var wwwPath = Path.Combine("..", "RawRealism", "www");
+        Directory.CreateDirectory(wwwPath);
+
+        // Bestandsnaam: GUID.txt
+        var fileName = $"{Guid.NewGuid()}.txt";
+        var filePath = Path.Combine(wwwPath, fileName);
+
+        // Inhoud: datum en tijd
+        var content = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss 'UTC'");
+
+        File.WriteAllText(filePath, content);
+
+        Console.WriteLine($"Bestand aangemaakt: {filePath}");
     }
 }
